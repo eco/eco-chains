7ff04dcfc5f7a146a5ce7aa5f91bfbc0326c2574
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-20 13:40:34 -0400

    addChain: adding superseed

---
+++ src/definitions/superseed.ts
@@ -0,0 +1,18 @@
+import { Chain } from 'viem'
+import { superseed as vsuperseed } from 'viem/chains'
+
+export const superseed: Chain = {
+  ...vsuperseed,
+  rpcUrls: {
+    ...vsuperseed.rpcUrls,
+    alchemy: {
+      http: ['https://superseed-mainnet.g.alchemy.com/v2'],
+    },
+  },
+  contracts: {
+    ...vsuperseed.contracts,
+    hyperlaneMailbox: {
+      address: '0x3a464f746D23Ab22155710f44dB16dcA53e0775E',
+    },
+  },
+}
