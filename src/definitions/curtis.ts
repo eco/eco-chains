1c98051c8e38dd7cc271cf4fec359caa486a02e1
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-21 15:25:18 -0400

    fix: update the muticall3 for curtis

--- src/definitions/curtis.ts
+++ src/definitions/curtis.ts
@@ -14,5 +14,9 @@ export const curtis: Chain = {
     metalayerRouter: {
       address: '0x6f23b0211056035a22430a10fd27ded8547dc377',
     },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 7290821,
+    },
   },
 }
