931074a3bbb667c637a3317d8ad33b626edf16dc
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-05-21 14:10:11 -0400

    fix: fixing url for celo

--- src/definitions/celo.ts
+++ src/definitions/celo.ts
@@ -4,12 +4,13 @@ import { celo as vcelo } from 'viem/chains'
 export const celo: Chain = {
   ...vcelo,
   rpcUrls: {
-    default: {
+    ...vcelo.rpcUrls,
+    quicknode: {
       http: [
-        'https://responsive-lingering-yard.celo-mainnet.quiknode.pro/${QUICKNODE_API_KEY}',
+        'https://cold-polished-violet.celo-mainnet.quiknode.pro/${QUICKNODE_API_KEY}',
       ],
       webSocket: [
-        'wss://responsive-lingering-yard.celo-mainnet.quiknode.pro/${QUICKNODE_API_KEY}',
+        'wss://cold-polished-violet.celo-mainnet.quiknode.pro/${QUICKNODE_API_KEY}',
       ],
     },
   },
