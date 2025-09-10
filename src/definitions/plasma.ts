d763a00e7e1c82a7e39a6baeb9ca2e0092a247ea
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2025-09-10 11:13:45 -0600

    Plasma beta (#92)
    * fix: adding plasma mainnet
    
    * adding custom verifier
    
    * add a fake usdc to plasma for testing

--- src/definitions/plasma.ts
+++ src/definitions/plasma.ts
@@ -29,3 +29,41 @@ export const plasmaTestnet = /*#__PURE__*/ defineChain({
     },
   },
 }) as EcoChain
+
+export const plasmaMainnet = /*#__PURE__*/ defineChain({
+  id: 9745,
+  name: 'Plasma Mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'XPL',
+    symbol: 'XPL',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.plasma.to'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Plasma Explorer',
+      url: 'https://plasmascan.to',
+    },
+  },
+  contracts: {
+    hyperlaneMailbox: {
+      address: '0x3a464f746D23Ab22155710f44dB16dcA53e0775E',
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 1,
+    },
+  },
+  isCalderaChain: false,
+  testnet: false,
+  stables: {
+    fUSDC: {
+      address: '0x8b27342BE5bB0DE1E9Bd361e216ee3D484d87D56',
+      decimals: 6,
+    },
+  },
+}) as EcoChain
