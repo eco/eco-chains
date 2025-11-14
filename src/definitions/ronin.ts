35de820b8e471dbd59b1c66de1e48e5b7b7724ef
Dirk <dpage@eco.com>
2025-11-14 16:30:40 -0600

    fix: Adding ronin chain (#103)
    * fix: Adding ronin chain
    
    * Ran prettier

---
+++ src/definitions/ronin.ts
@@ -0,0 +1,24 @@
+import { EcoChain } from '../chain.interface'
+import { ronin as vronin } from 'viem/chains'
+
+export const ronin: EcoChain = {
+  ...vronin,
+  id: 2020,
+  rpcUrls: {
+    ...vronin.rpcUrls,
+    alchemy: {
+      http: ['https://ronin-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}'],
+      webSocket: ['wss://ronin-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}'],
+    },
+  },
+  contracts: {
+    ...vronin.contracts,
+  },
+  isCalderaChain: false,
+  stables: {
+    USDC: {
+      address: '0x0b7007c13325c48911f73a2dad5fa5dcbf808adc',
+      decimals: 6,
+    },
+  },
+}
